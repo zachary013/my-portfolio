b1e279d8c3d96be6990c0ac6a8229f55d319edf4
Zakariae Azn <azarkanzakariae@gmail.com>
2024-09-13 11:59:23 +0100

    SEO

--- app/page.jsx
+++ app/page.jsx
@@ -48,7 +48,7 @@ const Home = () => {
             </div>
           </div>
           <div className="order-1 xl:order-none mb-8 xl:mb-0">
-            <Photo />
+            <Photo alt="my photo"/>
           </div>
         </div>
       </div>
